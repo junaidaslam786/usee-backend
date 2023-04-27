bfb1d352bffd8ceb3951b966e3f8a55f548f880e
mkam22 <muhammad.kamran@invozone.com>
2023-04-27 11:52:29 +0500

    updated code for property remove request

--- src/app/admin/property/property.controller.js
+++ src/app/admin/property/property.controller.js
@@ -18,3 +18,39 @@ export const listProperties = async (req, res, next) => {
     return next(err);
   }
 };
+
+/**
+ * GET /admin/property/list
+ * List all properties
+ */
+export const listPropertyRemovalRequest = async (req, res, next) => {
+  try {
+    const result = await propertyService.listPropertyRemovalRequest(req.dbInstance);
+    if (result?.error && result?.message) {
+        return next(createError(400, result.message));
+    }
+
+    return res.status(200).json(result);
+  } catch (err) {
+    console.log('listPropertyError', err); 
+    return next(err);
+  }
+};
+
+/**
+ * POST /admin/property/list
+ * List all properties
+ */
+export const approvePropertyRemovalRequest = async (req, res, next) => {
+  try {
+    const result = await propertyService.approvePropertyRemovalRequest(req.body, req.dbInstance);
+    if (result?.error && result?.message) {
+        return next(createError(400, result.message));
+    }
+
+    return res.status(200).json({result, message: "Property removed request approved successfully"});
+  } catch (err) {
+    console.log('removePropertyError', err);
+    return next(err);
+  }
+};
--- src/app/admin/property/property.service.js
+++ src/app/admin/property/property.service.js
@@ -1,3 +1,6 @@
+import { PRODUCT_STATUS } from '@/config/constants';
+import { Op } from 'sequelize';
+
 export const listProperties = async (dbInstance) => {
   try {
     const { count, rows } = await dbInstance.product.findAndCountAll({
@@ -19,6 +22,69 @@ export const listProperties = async (dbInstance) => {
     };
   } catch (err) {
     console.log('listPropertiesServiceError', err);
+    return { error: true, message: 'Server not responding, please try again later.' };
+  }
+};
+
+export const listPropertyRemovalRequest = async (dbInstance) => {
+  try {
+    const { count, rows } = await dbInstance.productRemoveRequest.findAndCountAll({
+      include: [
+        {
+          model: dbInstance.product,
+          as: 'product',
+          where: { status: { [Op.ne]: 'sold' } },
+          include: [
+            {
+              model: dbInstance.user,
+              as: 'user',
+              where: { deletedAt: null },
+              attributes: ['firstName', 'lastName', 'email', 'phoneNumber', 'profileImage'],
+            },
+          ],
+        },
+
+      ],
+
+      order: [['id', 'DESC']],
+    });
+
+    return {
+      data: rows,
+      totalItems: count,
+    };
+  } catch (err) {
+    console.log('listPropertiesServiceError', err);
+    return { error: true, message: 'Server not responding, please try again later.' };
+  }
+};
+
+export const getPropertyById = async (propertyId, dbInstance) => {
+  const property = await dbInstance.product.findOne({
+    where: { id: propertyId },
+  });
+  if (!property) {
+    return false;
+  }
+
+  return property;
+};
+
+export const approvePropertyRemovalRequest = async (reqBody, dbInstance) => {
+  try {
+    const { propertyId, reasonId } = reqBody;
+
+    const property = await getPropertyById(propertyId, dbInstance);
+    if (!property) {
+      return { error: true, message: 'Invalid property id or Property do not exist.' };
+    }
+
+    property.status = reasonId === 1 ? PRODUCT_STATUS.SOLD : PRODUCT_STATUS.REMOVED;
+
+    await property.save();
+    return true;
+  } catch (err) {
+    console.log('approvePropertyRemovalServiceError', err);
     return { error: true, message: 'Server not responding, please try again later.' };
   }
 };
