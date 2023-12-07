21a698491f4487ef6f2679e0bc37fce6b687d753
Ali Haider <a.haider_5@hotmail.com>
2023-12-07 20:29:59 +0500

    - converts report routes to POST

--- src/app/superAdmin/reports/reports.route.js
+++ src/app/superAdmin/reports/reports.route.js
@@ -3,12 +3,8 @@ import * as analyticsController from './reports.controller';
 
 const router = Router();
 
-// router.get('/users', analyticsController.getUsersAnalytics);
-// router.get('/properties', analyticsController.getActiveUsersAnalytics);
-// router.get('/services', analyticsController.getNonActiveUsersAnalytics);
-
-router.get('/users', analyticsController.getUsersData);
-router.get('/properties', analyticsController.getPropertiesData);
-router.get('/services', analyticsController.getServicesData);
+router.post('/users', analyticsController.getUsersData);
+router.post('/properties', analyticsController.getPropertiesData);
+router.post('/services', analyticsController.getServicesData);
 
 export default router;
