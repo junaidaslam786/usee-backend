2de14bba9a828d3e32c756e9868cc09de1536bd7
Ali Haider <a.haider_5@hotmail.com>
2023-12-11 17:03:46 +0500

    - Adds createPropertyRules validation rules

--- src/app/property/property.request.js
+++ src/app/property/property.request.js
@@ -17,3 +17,16 @@ export const customerOfferRequestRules = [
   body('amount').exists().withMessage('Please provide amount').notEmpty().withMessage('Please provide amount'),
   body('notes').optional(),
 ];
+
+export const createPropertyRules = [
+  body('title').exists().withMessage('Please provide property title').notEmpty().withMessage('Please provide property title'),
+  body('description').exists().withMessage('Please provide property description').notEmpty().withMessage('Please provide property description'),
+  body('price').exists().withMessage('Please provide property price').notEmpty().withMessage('Please provide property price'),
+  body('address').exists().withMessage('Please provide property address').notEmpty().withMessage('Please provide property address'),
+  body('city').exists().withMessage('Please provide property city').notEmpty().withMessage('Please provide property city'),
+  body('postalCode').exists().withMessage('Please provide property postal code').notEmpty().withMessage('Please provide property postal code'),
+  body('region').exists().withMessage('Please provide property region').notEmpty().withMessage('Please provide property region'),
+  body('latitude').exists().withMessage('Please provide property latitude').notEmpty().withMessage('Please provide property latitude'),
+  body('longitude').exists().withMessage('Please provide property longitude').notEmpty().withMessage('Please provide property longitude'),
+  body('virtualTourType').exists().withMessage('Please provide property virtual tour type').notEmpty().withMessage('Please provide property virtual tour type'),
+];
