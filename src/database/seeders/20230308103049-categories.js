c4fe740cfeadec809fc0d1ff6df46407dcb6773d
Ali Haider <a.haider_5@hotmail.com>
2024-03-11 15:58:47 +0500

    - Adds label changes and code fixes to categories seeder

--- src/database/seeders/20230308103049-categories.js
+++ src/database/seeders/20230308103049-categories.js
@@ -1,98 +1,98 @@
 'use strict';
 
 module.exports = {
-  async up (queryInterface, Sequelize) {
+  async up(queryInterface, Sequelize) {
     try {
       const categories = [
         {
           id: 1,
-          name: "Property"
-        }
+          name: 'Property',
+        },
       ];
       await queryInterface.bulkInsert('categories', categories);
-  
+
       const categoryFields = [
         {
           id: 1,
           category_id: 1,
-          label: "Property Type",
-          type: "select",
-          options: "commercial,residential",
+          label: 'Property Type',
+          type: 'select',
+          options: 'commercial,residential',
           required: true,
           order: 1,
         },
         {
           id: 2,
           category_id: 1,
-          label: "Property Category Type",
-          type: "select",
-          options: "rent,sale",
+          label: 'Property Category Type',
+          type: 'select',
+          options: 'rent,sale',
           required: true,
           order: 2,
         },
         {
           id: 3,
           category_id: 1,
-          label: "Unit",
-          type: "select",
-          options: "sq_ft,sq_mt",
+          label: 'Unit',
+          type: 'select',
+          options: 'sq_ft,sq_mt',
           required: false,
           order: 3,
         },
         {
           id: 4,
           category_id: 1,
-          label: "Area",
-          type: "text",
-          options: "",
+          label: 'Area',
+          type: 'text',
+          options: '',
           required: false,
           order: 4,
         },
         {
           id: 5,
           category_id: 1,
-          label: "No. of bedrooms",
-          type: "select",
-          options: "1,2,3,4,5,6,7,8,9,10",
+          label: 'No. of bedrooms',
+          type: 'select',
+          options: '1,2,3,4,5,6,7,8,9,10',
           required: false,
           order: 5,
         },
         {
           id: 6,
           category_id: 1,
-          label: "Residential Properties",
-          type: "select",
-          options: "apartment,house,bungalow,studio,room,duplex,triplex,cottage",
+          label: 'Residential Property Type',
+          type: 'select',
+          options: 'apartment,house,bungalow,studio,room,duplex,triplex,cottage',
           required: false,
           order: 6,
         },
         {
           id: 7,
           category_id: 1,
-          label: "Commercial Properties",
-          type: "select",
-          options: "office,retail,shop,shopping_center,store,hotels,club,restaurant,hotel_room",
+          label: 'Commercial Property Type',
+          type: 'select',
+          options: 'office,retail,shop,shopping_center,store,hotels,club,restaurant,hotel_room',
           required: false,
           order: 7,
         },
         {
           id: 8,
           category_id: 1,
-          label: "Price Type",
-          type: "select",
-          options: "weekly,monthly,yearly",
+          label: 'Price Type',
+          type: 'select',
+          options: 'weekly,monthly,yearly',
           required: false,
           order: 8,
-        }
+        },
       ];
       await queryInterface.bulkInsert('category_fields', categoryFields);
-    } catch(error) {
+    } catch (error) {
       console.log('error', error);
     }
   },
 
-  async down (queryInterface, Sequelize) {
+  async down(queryInterface, Sequelize) {
     await queryInterface.bulkDelete('categories', null, {});
     await queryInterface.bulkDelete('category_fields', null, {});
-  }
+  },
 };
